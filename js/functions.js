749503d1b275281ebf61b28279d5fc632b891577
CarlosMora9920 <cmora992071@gmail.com>
2023-11-21 23:36:01 -0500

    Funcion JS
    se creo funcion de JS para el llamado de formularios en uno solo

---
+++ js/functions.js
@@ -0,0 +1,29 @@
+function cargarFormulario(nameForm) {
+   
+    var url = 'libros.html';
+
+    // Obtener el elemento por su ID
+    var limpiar = document.getElementById(nameForm);
+
+    // Limpiar el contenido utilizando innerHTML
+    limpiar.innerHTML = '';
+
+
+    // Realizar una solicitud de red mediante fetch
+    fetch(url)
+        .then(response => response.text())  // Obtener el contenido del archivo como texto
+        .then(data => {
+            // Insertar el contenido del formulario en el contenedor
+            document.getElementById(nameForm).innerHTML = data;
+        })
+        .catch(error => console.error('Error al cargar el formulario:', error));
+
+
+    
+
+
+
+}
+
+
+
