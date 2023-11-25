c4f3df057e9bebda6a775c54a0fb10744a6b5213
CarlosMora9920 <cmora992071@gmail.com>
2023-11-25 12:59:54 -0500

    cambios realizados

--- js/functions.js
+++ js/functions.js
@@ -20,5 +20,51 @@ function cargarFormulario(nameForm,url) {
 
 }
 
+function iniciarSesion() {
+    let usuario = document.getElementById("usuario").value;
+    let contra = document.getElementById("contra").value;
+    
+    var usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+    console.log(usuarios);
+    var usuarioLog = usuarios.filter(function(usuarioF){
+        return (usuarioF["usuario"] == usuario);
+    });
 
+    if(usuarioLog.length == 0){
+        alert("Usuario no encontrado");
+        return;
+    }
+
+    if(usuarioLog[0]["contra"] != contra){
+        alert("Contraseña incorrecta");
+        return;
+    }
+    localStorage.setItem("usuario", JSON.stringify(usuarioLog[0]));
+    window.location.href = "../pages/home.html";
+}
+
+function registroLibro(){
+    let n_book = document.getElementById("name-book").value;
+    let autor = document.getElementById("book-author").value;
+    let editorial = document.getElementById("book-editorial").value;
+    let y_public = document.getElementById("book-publication").value;
+    let genero = document.getElementById("book-gender").value;
+
+    var book = JSON.parse(localStorage.getItem("book")) || [];
+       //alerta(book);
+    var bookLog = book.filter(function(books_f){
+        return (books_f["name-book"] == n_book);
+    });
+
+    if(bookLog.length > 0){
+        alert("Usuario ya existe");
+        return;
+    }
+    let booksT = { "name-book" : n_book,"book-author":autor, "book-editorial":editorial, "book-publication" : y_public, "book-gender" :genero }
+    book.push(booksT);
+
+    localStorage.setItem("book", JSON.stringify(book));
+    alert("Registro completo");
+    document.getElementById("formRegistro").reset();
+}
 
