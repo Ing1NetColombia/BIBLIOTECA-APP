85a563f716ad85f83b630207b465070b2f3a8b97
CarlosMora9920 <cmora992071@gmail.com>
2023-12-04 19:55:53 -0500

    implentacion del sweetalert

--- js/functions.js
+++ js/functions.js
@@ -1,29 +1,26 @@
-function cargarFormulario(nameForm,url) {
-   
-    //declaracion de variables para el llamado de los formularios
-    //var url = 'libros.html';
+function cargarFormulario(nameForm, url) {
+  //declaracion de variables para el llamado de los formularios
+  //var url = 'libros.html';
 
-    // Obtener el elemento por su ID
-    var limpiar = document.getElementById(nameForm);
+  // Obtener el elemento por su ID
+  var limpiar = document.getElementById(nameForm);
 
-    // Limpiar el contenido utilizando innerHTML
-    limpiar.innerHTML = '';
+  // Limpiar el contenido utilizando innerHTML
+  limpiar.innerHTML = "";
 
-    // Realizar una solicitud de red mediante fetch
-    fetch(url)
-        .then(response => response.text())  // Obtener el contenido del archivo como texto
-        .then(data => {
-            // Insertar el contenido del formulario en el contenedor
-            document.getElementById(nameForm).innerHTML = data;
+  // Realizar una solicitud de red mediante fetch
+  fetch(url)
+    .then((response) => response.text()) // Obtener el contenido del archivo como texto
+    .then((data) => {
+      // Insertar el contenido del formulario en el contenedor
+      document.getElementById(nameForm).innerHTML = data;
 
-            var datosGuardados = JSON.parse(localStorage.getItem("book"));
+      var datosGuardados = JSON.parse(localStorage.getItem("book"));
 
-            switch (url) {
-                case "catalago.html":
-                    
-                datosGuardados.forEach(element => {
-                    
-                    var plantilla = `<div class="container col-xxl-8 px-4 py-5">
+      switch (url) {
+        case "catalago.html":
+          datosGuardados.forEach((element) => {
+            var plantilla = `<div class="container col-xxl-8 px-4 py-5">
                         <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
                         <div class="col-10 col-sm-8 col-lg-6">
                             <img src="./assets/libro2.png" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
@@ -41,18 +38,15 @@ function cargarFormulario(nameForm,url) {
                         </div>
                         </div>
                     
-                         <div class="b-example-divider"></div>`
-                
-                        document.getElementById("libroteca").innerHTML += plantilla;    
+                         <div class="b-example-divider"></div>`;
 
-                   });
+            document.getElementById("libroteca").innerHTML += plantilla;
+          });
 
-                    break;
-                case "reserva.html":
-
-                   datosGuardados.forEach(element => {
-
-                    var reservados = ` 
+          break;
+        case "reserva.html":
+          datosGuardados.forEach((element) => {
+            var reservados = ` 
                     <div class="col">
                       <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" style="background-image: url('unsplash-photo-1.jpg');">
                         <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
@@ -70,30 +64,68 @@ function cargarFormulario(nameForm,url) {
                           </ul>
                         </div>
                       </div>
-                    `
- 
-                    document.getElementById("ticket").innerHTML += reservados;    
-                    
+                    `;
 
-                   })
+            document.getElementById("ticket").innerHTML += reservados;
+          });
 
-                    break;
-            
-                default:
-                    break;
-            }
+          break;
 
+        case "historial.html":
 
+        var datosGuardados = JSON.parse(localStorage.getItem("reserva"));
 
-            if (url == "catalago.html"){
-                var datosGuardados = JSON.parse(localStorage.getItem("book"));
-                console.log(datosGuardados)
-            //    alert(datosGuardados)
-                
+          document.getElementById("historial").innerHTML = ` 
+                <div id = "historial" class="row">
+                      <div class="col-md-12">
+                      <div class="table-wrap">
+                        <table class="table">
+                          <thead class="thead-dark">
+                            <tr>
+                              <th>Libro</th>
+                              <th>Autor</th>
+                              <th>Editorial</th>
+                              <th>Fecha de reserva</th>
+                              <th>Usuario</th>
+                            </tr>
+                          </thead>
+                          <tbody id="historialTablaBody">
+                            
+                          </tbody>
+                        </table>
+                      </div>
+                    </div>
+                </div>
+                  
+                `;
 
-                datosGuardados.forEach(element => {
-                    
-                    var plantilla = `<div class="container col-xxl-8 px-4 py-5">
+          datosGuardados.forEach((element) => {
+            var historial = `<tr>
+            <td>${element["name-book"]} </td>
+            <td>${element["book-author"]} </td>
+            <td>${element["book-editorial"]} </td>
+            <td>${element["fecha_reserva"]} </td>
+            </tr>
+           
+            `;
+
+            document.getElementById("historialTablaBody").innerHTML +=
+              historial;
+          });
+
+          break;
+
+        default:
+          break;
+      }
+
+      if (url == "catalago.html") {
+        var datosGuardados = JSON.parse(localStorage.getItem("book"));
+        console.log(datosGuardados);
+        //    alert(datosGuardados)
+
+        datosGuardados.forEach((element) => {
+          var plantilla = `<div class="container col-xxl-8 px-4 py-5">
                     <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
                       <div class="col-10 col-sm-8 col-lg-6">
                         <img src="./assets/libro2.png" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
@@ -111,17 +143,13 @@ function cargarFormulario(nameForm,url) {
                     </div>
                   </div>
                 
-                  <div class="b-example-divider"></div>`
-                
-                    document.getElementById("libroteca").innerHTML += plantilla;    
+                  <div class="b-example-divider"></div>`;
 
-                });
-
-            }
-
-        })
-        .catch(error => console.error('Error al cargar el formulario:', error));
-
+          document.getElementById("libroteca").innerHTML += plantilla;
+        });
+      }
+    })
+    .catch((error) => console.error("Error al cargar el formulario:", error));
 }
 
 function iniciarSesion() {
@@ -132,9 +160,9 @@ function iniciarSesion() {
   var userLog = null;
 
   if (users.length > 0) {
-    users.forEach(function(usuarios_f) {
+    users.forEach(function (usuarios_f) {
       var nombreUsuario = usuarios_f["usuario_init"];
-      
+
       if (nombreUsuario === usuario) {
         userLog = usuarios_f;
         return;
@@ -159,7 +187,7 @@ function iniciarSesion() {
   console.log("Antes de la redirección");
 
   // var local = window.location.href
-  
+
   // Cambiar la ubicación (URL) a "login_2.html"
   window.location.href = "../index.html";
   // alert("ingresando")
@@ -168,59 +196,68 @@ function iniciarSesion() {
   // console.log("Después de la redirección");
 }
 
-
-
-function registroUsuarios(){
+function registroUsuarios() {
   let usuarios = document.getElementById("usuario_init").value;
   let password = document.getElementById("contra_init").value;
   let email = document.getElementById("Email").value;
 
   var user = JSON.parse(localStorage.getItem("userlog")) || [];
-       //alerta(book);
-    var userLog = user.filter(function(user_f){
-        return (user_f["user"] == usuarios);
-    });
-    alert(usuarios)
-   if(userLog.length > 0){
-      alert("Usuario ya existe");
-      return;
+  //alerta(book);
+  var userLog = user.filter(function (user_f) {
+    return user_f["user"] == usuarios;
+  });
+  alert(usuarios);
+  if (userLog.length > 0) {
+    alert("Usuario ya existe");
+    return;
   }
-  let user_r = { "usuario_init" : usuarios,"contra_init":password,"Email" : email}
+  let user_r = { usuario_init: usuarios, contra_init: password, Email: email };
   user.push(user_r);
 
   localStorage.setItem("user", JSON.stringify(user));
-    alert("Registro completo");
-    document.getElementById("incio").reset();
-
+  alert("Registro completo");
+  document.getElementById("incio").reset();
 }
 
+function registroLibro() {
+  let n_book = document.getElementById("name-book").value;
+  let autor = document.getElementById("book-author").value;
+  let editorial = document.getElementById("book-editorial").value;
+  let y_public = document.getElementById("book-publication").value;
+  let genero = document.getElementById("book-gender").value;
 
-function registroLibro(){
-    let n_book = document.getElementById("name-book").value;
-    let autor = document.getElementById("book-author").value;
-    let editorial = document.getElementById("book-editorial").value;
-    let y_public = document.getElementById("book-publication").value;
-    let genero = document.getElementById("book-gender").value;
+  var book = JSON.parse(localStorage.getItem("book")) || [];
+  //alerta(book);
+  var bookLog = book.filter(function (books_f) {
+    return books_f["name-book"] == n_book;
+  });
 
-    var book = JSON.parse(localStorage.getItem("book")) || [];
-       //alerta(book);
-    var bookLog = book.filter(function(books_f){
-        return (books_f["name-book"] == n_book);
-    });
+  if (bookLog.length > 0) {
+    alert("Usuario ya existe");
+    return;
+  }
+  let booksT = {
+    "name-book": n_book,
+    "book-author": autor,
+    "book-editorial": editorial,
+    "book-publication": y_public,
+    "book-gender": genero,
+  };
+  book.push(booksT);
 
-    if(bookLog.length > 0){
-        alert("Usuario ya existe");
-        return;
-    }
-    let booksT = { "name-book" : n_book,"book-author":autor, "book-editorial":editorial, "book-publication" : y_public, "book-gender" :genero }
-    book.push(booksT);
+  localStorage.setItem("book", JSON.stringify(book));
+  Swal.fire({
+    title: "Registro Completa",
+    text: "Puedes continuar con el proceso",
+    icon: "success"
+  });
 
-    localStorage.setItem("book", JSON.stringify(book));
-    alert("Registro completo");
-    document.getElementById("formRegistro").reset();
+
+  //alert("Registro completo");
+  document.getElementById("formRegistro").reset();
 }
 
-function reservarLibro(){
+function reservarLibro() {
   let name_book1 = document.getElementById("name-book");
   let name_book = name_book1.innerText;
   let autor1 = document.getElementById("book-author");
@@ -228,25 +265,29 @@ function reservarLibro(){
   let editorial1 = document.getElementById("book-editorial");
   let editorial = editorial1.innerText;
   let fecha = new Date();
-  let fechaReserva = fecha.toISOString(); 
-
+  let fechaReserva = fecha.toISOString();
 
   var reserva = JSON.parse(localStorage.getItem("reserva")) || [];
-     
-  alert(name_book)
 
-  var ReservaLog = reserva.filter(function(reserva_l){
-      return (reserva_l["name-book"] == name_book);
+  alert(name_book);
+
+  var ReservaLog = reserva.filter(function (reserva_l) {
+    return reserva_l["name-book"] == name_book;
   });
 
-  if(ReservaLog.length > 0){
-      alert("Usuario ya existe");
-      return;
+  if (ReservaLog.length > 0) {
+    alert("Usuario ya existe");
+    return;
   }
-  let ReservaL = { "name-book" : name_book,"book-author":autor, "book-editorial":editorial,"fecha_reserva" :fechaReserva }
+  let ReservaL = {
+    "name-book": name_book,
+    "book-author": autor,
+    "book-editorial": editorial,
+    fecha_reserva: fechaReserva,
+  };
   reserva.push(ReservaL);
 
-  alert(JSON.stringify(ReservaL));  
+  alert(JSON.stringify(ReservaL));
   localStorage.setItem("reserva", JSON.stringify(reserva));
   alert("Registro completo");
   //document.getElementById("formRegistro").reset();
@@ -257,7 +298,7 @@ function cargarHistorial() {
   var tablaBody = document.getElementById("historialTablaBody");
   var datosGuardados = JSON.parse(localStorage.getItem("reserva"));
 
-  datosGuardados.forEach(element => {
+  datosGuardados.forEach((element) => {
     var fila = document.createElement("tr");
 
     var celdaLibro = document.createElement("td");
@@ -265,11 +306,11 @@ function cargarHistorial() {
     fila.appendChild(celdaLibro);
 
     var celdaAutor = document.createElement("td");
-    celdaAutor.textContent = element["book-author"];  // Corregido el nombre de la variable
+    celdaAutor.textContent = element["book-author"]; // Corregido el nombre de la variable
     fila.appendChild(celdaAutor);
 
     var celdaEditorial = document.createElement("td");
-    celdaEditorial.textContent = element["book-editorial"];  // Corregido el nombre de la variable
+    celdaEditorial.textContent = element["book-editorial"]; // Corregido el nombre de la variable
     fila.appendChild(celdaEditorial);
 
     var celdaFecha = document.createElement("td");
@@ -279,6 +320,3 @@ function cargarHistorial() {
     tablaBody.appendChild(fila);
   });
 }
-
-
-
